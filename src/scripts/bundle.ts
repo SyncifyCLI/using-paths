91709d939edfc189b47d328d2405d9b56af7d78d
Nikolas Savvidis <nicos@gmx.com>
2024-10-11 17:45:39 +0200

    initial commit

---
+++ src/scripts/bundle.ts
@@ -0,0 +1,11 @@
+import { bar } from './components/bar';
+import { foo } from './components/foo';
+
+function example () {
+
+  foo('example of foo');
+  bar('example of bar');
+
+}
+
+export default example();
